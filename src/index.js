d1da26702dc16d2814e28c82985d051e109cd7d7
DOROTHY CHEPKOECH <chepkoechdorothy209@gmail.com>
2025-03-16 16:51:39 +0300

    Project Tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit',(e) =>{
+    e.preventDefault();
+    const task = e.target.elements["new-task-description"].value;
+    const priority = e.target.elements["priority"].value; 
+
+    taskDescription(task, priority); // Pass priority
+
+    form.reset();
+  });
+});  // your code here
+function taskDescription(task,priority){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click',deleteTask)
+  p.textContent = `${task} ` ;
+  btn.textContent = 'x'
+  p.appendChild(btn)
+  if (priority === 'high') {
+    p.style.color = 'red';
+  } else if (priority === 'medium') {
+    p.style.color = 'yellow';
+  } else {
+    p.style.color = 'green';
+  }
+
+  console.log(p);
+  document.querySelector('#tasks').appendChild(p);
+}
+
+
+
+
+function deleteTask(e){
+  e.target.parentNode.remove();
+
+}
